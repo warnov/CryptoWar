a705470740947940211028bf2b873a26e0da1336
warnov <warnov@gmail.com>
2019-09-07 13:45:56 -0500

    Added project to support NetStandard. The WinForms client was updated in its target framework to 4.7 to support netstandard package for testing

--- Crypto RMWin/Form1.cs
+++ Crypto RMWin/Form1.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Windows.Forms;
-using WarNov.AesManagedWrapper.Win32;
+using WarNov.AESManagedWrapper.NetStandard;
 
 namespace CryptoRMWin
 {
---
+++ WarNov.AESManagedWrapper.CoreConsoleTester/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using WarNov.AESManagedWrapper.NetStandard;
+
+
+namespace WarNov.AESManagedWrapper.CoreConsoleTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ss = "alejandi";
+            var encTxt = RMWCrypto.EncryptStringAES("13958291###soijfsdndn^^&&2342##", ss);
+            Console.WriteLine(encTxt);
+            var decTxt = RMWCrypto.DecryptStringAES(encTxt, ss);
+            Console.WriteLine(decTxt);
+        }
+    }
+}
